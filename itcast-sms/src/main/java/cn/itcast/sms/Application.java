16e01c29bdf60ce801c045040a1860f0e33766f6
wanzi <1024819967@qq.com>
2018-09-17 00:45:11 +0800

    验证短信微服务

---
+++ itcast-sms/src/main/java/cn/itcast/sms/Application.java
@@ -0,0 +1,16 @@
+package cn.itcast.sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Date:2018-09-15
+ * Author:Wanzi
+ * Desc:
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+}
